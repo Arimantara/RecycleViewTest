342dedc9303db6f60fd6d337c040d70eaa9aaeff
I Gusti Agung Arimantara <gung.ara75@yahoo.com>
2022-03-21 21:21:27 +0800

    Initial commit

---
+++ app/src/main/java/com/example/recycleviewtest/BookActivity.java
@@ -0,0 +1,35 @@
+package com.example.recycleviewtest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BookActivity extends AppCompatActivity {
+
+    private TextView tvtitle, tvdescription, tvcategory;
+    private ImageView img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_book);
+
+        tvtitle = (TextView) findViewById(R.id.txtTitle);
+        tvdescription = (TextView) findViewById(R.id.txtDesc);
+        tvcategory = (TextView) findViewById(R.id.txtCat);
+        img = (ImageView) findViewById(R.id.bookThumbnail);
+
+        Intent intent = getIntent();
+        String Title = intent.getExtras().getString("Title");
+        String Description = intent.getExtras().getString("Description");
+        int image = intent.getExtras().getInt("Thumbnail");
+
+        tvtitle.setText(Title);
+        tvdescription.setText(Description);
+        img.setImageResource(image);
+
+    }
+}
